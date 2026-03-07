8afcab6b5550f575ea78d79dd0ef33b173760b69
Wulvv <18334624060@163.com>
2026-03-07 21:34:49 +0800

    20260307

---
+++ 20IsValid.cpp
@@ -0,0 +1,114 @@
+/* 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+ 
+
+示例 1：
+
+输入：s = "()"
+
+输出：true
+
+示例 2：
+
+输入：s = "()[]{}"
+
+输出：true
+
+示例 3：
+
+输入：s = "(]"
+
+输出：false
+
+示例 4：
+
+输入：s = "([])"
+
+输出：true
+
+示例 5：
+
+输入：s = "([)]"
+
+输出：false
+
+ 
+
+提示：
+
+1 <= s.length <= 104
+s 仅由括号 '()[]{}' 组成 */
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c == '('||c == '{'||c == '['){
+            st.push(c);
+        }
+        else{
+            if(st.empty()) return false;
+            char top = st.top();
+            st.pop();
+            if(c == ')'&& top!='('){
+                return false;
+            }
+            if(c == '}'&& top!='{'){
+                return false;
+            }if(c == ']'&& top!='['){
+                return false;
+            }
+            //if(st.empty()) return false;
+        }
+    }
+    return st.empty();
+    }
+};
+
+
+
+
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        unordered_map<char,char> mp={    //c++中的map需要导入map库，这里是哈希表版本的map
+            {')','('},                   //定义方式：map<key类型, value类型> 名字;二者相互对应
+            {']','['},                   //value = mp[key]
+            {'}','{'}
+        };
+
+        for(char c:s){
+
+            // 左括号
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+
+            // 右括号
+            else{
+
+                if(st.empty()) return false;
+
+                if(st.top()!=mp[c]) return false;       //map访问方式mp['(']
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
--- 387FirstUniqChar.cpp
+++ 387FirstUniqChar.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
-int getindex(string s,char c){
-    for(int i = 0;i<s.size();i++){
-        if(c==s[i]){
-            return i;
-        }
-    }
-    return -1;
-}
+
 int main(){
     int count[26]={0};//数组初始化
     string s;
     getline(cin,s);
-    for(int i = 0;i<s.size();i++){
-        if(s[i]>='a'&&s[i]<='z'){//排除出现大写字母的可能
-            count[s[i]-'a']++;
-        }
+    for(char c:s){
+        count[c-'a']++;
     }
-
-    for(int i = 0;i<26;i++){
-        if(count[i]==1){
-            cout<<getindex(s,i+'a');
+    for(int i = 0; i<s.size();i++){
+        if(count[s[i]-'a'] == 1){
+            cout<<i<<endl;
             return 0;
         }
     }
-
-    return 0;
-}
+}//这道题字符串只包含小写字母，如果包含任意字符，count[256]，可以处理大小写字母、数字、符号
